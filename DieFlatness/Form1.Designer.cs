c9865b1181290e289327b244dcfedec4536fe09c
V <V@LAPTOP-66GCD269>
2019-05-13 08:45:13 +0800

    1

--- DieFlatness/Form1.Designer.cs
+++ DieFlatness/Form1.Designer.cs
@@ -29,11 +29,203 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cbClient = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.MainDisplay = new Cognex.VisionPro.CogRecordDisplay();
+            this.LiveDisplay = new Cognex.VisionPro.CogRecordDisplay();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDisplay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveDisplay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.btn3);
+            this.splitContainer1.Panel1.Controls.Add(this.cbClient);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(983, 554);
+            this.splitContainer1.SplitterDistance = 135;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // cbClient
+            // 
+            this.cbClient.AutoSize = true;
+            this.cbClient.Location = new System.Drawing.Point(893, 12);
+            this.cbClient.Name = "cbClient";
+            this.cbClient.Size = new System.Drawing.Size(78, 16);
+            this.cbClient.TabIndex = 1;
+            this.cbClient.Text = "checkBox1";
+            this.cbClient.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "初始化";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.MainDisplay);
+            this.splitContainer2.Panel1.Controls.Add(this.LiveDisplay);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.label2);
+            this.splitContainer2.Panel2.Controls.Add(this.label1);
+            this.splitContainer2.Size = new System.Drawing.Size(983, 415);
+            this.splitContainer2.SplitterDistance = 750;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // MainDisplay
+            // 
+            this.MainDisplay.ColorMapLowerClipColor = System.Drawing.Color.Black;
+            this.MainDisplay.ColorMapLowerRoiLimit = 0D;
+            this.MainDisplay.ColorMapPredefined = Cognex.VisionPro.Display.CogDisplayColorMapPredefinedConstants.None;
+            this.MainDisplay.ColorMapUpperClipColor = System.Drawing.Color.Black;
+            this.MainDisplay.ColorMapUpperRoiLimit = 1D;
+            this.MainDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainDisplay.DoubleTapZoomCycleLength = 2;
+            this.MainDisplay.DoubleTapZoomSensitivity = 2.5D;
+            this.MainDisplay.Location = new System.Drawing.Point(0, 0);
+            this.MainDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MainDisplay.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
+            this.MainDisplay.MouseWheelSensitivity = 1D;
+            this.MainDisplay.Name = "MainDisplay";
+            this.MainDisplay.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("MainDisplay.OcxState")));
+            this.MainDisplay.Size = new System.Drawing.Size(750, 415);
+            this.MainDisplay.TabIndex = 3;
+            // 
+            // LiveDisplay
+            // 
+            this.LiveDisplay.ColorMapLowerClipColor = System.Drawing.Color.Black;
+            this.LiveDisplay.ColorMapLowerRoiLimit = 0D;
+            this.LiveDisplay.ColorMapPredefined = Cognex.VisionPro.Display.CogDisplayColorMapPredefinedConstants.None;
+            this.LiveDisplay.ColorMapUpperClipColor = System.Drawing.Color.Black;
+            this.LiveDisplay.ColorMapUpperRoiLimit = 1D;
+            this.LiveDisplay.DoubleTapZoomCycleLength = 2;
+            this.LiveDisplay.DoubleTapZoomSensitivity = 2.5D;
+            this.LiveDisplay.Location = new System.Drawing.Point(0, 0);
+            this.LiveDisplay.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LiveDisplay.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
+            this.LiveDisplay.MouseWheelSensitivity = 1D;
+            this.LiveDisplay.Name = "LiveDisplay";
+            this.LiveDisplay.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("LiveDisplay.OcxState")));
+            this.LiveDisplay.Size = new System.Drawing.Size(214, 287);
+            this.LiveDisplay.TabIndex = 4;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(149, 34);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 56);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "开始采集";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(267, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 56);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "配置";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(19, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "图片宽";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(70, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 554);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.MainDisplay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LiveDisplay)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private Cognex.VisionPro.CogRecordDisplay MainDisplay;
+        private Cognex.VisionPro.CogRecordDisplay LiveDisplay;
+        private System.Windows.Forms.CheckBox cbClient;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- DieFlatness/Form1.cs
+++ DieFlatness/Form1.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.Implementation.Internal;
+using Cognex.VisionPro.ToolBlock;
+using GeneralComponent;
+using SmartRayImageSource;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +18,311 @@ namespace DieFlatness
 {
     public partial class Form1 : Form
     {
+        GeneralComponent.TcpServer MyTCPServer = null;
+        Random rand = new Random();
+
+        List<AuthControlHelper> AuthoritionHelper = new List<AuthControlHelper>();
+        enum SensorStatus
+        {
+            Idle,
+            Scaning,
+            Aborting
+
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        CogToolBlock visionProAlg;
+        ImageSource SmartRayImageSource;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MyTCPServer == null)
+            {
+                MyTCPServer = new TcpServer();
+                this.MyTCPServer.Port = 2000;
+                this.MyTCPServer.DataType = TcpServer.StreamType.Text;
+                this.MyTCPServer.OnConnect += Connection2_OnConnect;
+                this.MyTCPServer.OnLeaving += Connection2_OnLeaving;
+
+                MyTCPServer.Open();
+
+            }
+            else
+            {
+       
+            }
+
+            SmartRayImageSource.ImageSource srIs = new ImageSource();
+
+            if (srIs.Configure(Application.StartupPath+"/ScanSmartRay_2/SmartRayImageSource.xml"))
+            {
+                SmartRayImageSource = srIs;
+
+                SmartRayImageSource.OnImage += ImageSource_OnImage;
+            }
+
+            if (srIs != null)
+            {
+                srIs.OnPackageImage += SrIs_OnPackageImage;
+            }
+            // this.MainDisplay.ColorMapLoad(Application.StartupPath + "/ColorMap.map");
+
+
+            NotifierHelper.Notify("App.Init.Sucess", "完成初始化。");
+           
+            //}
+
+
+            {
+                visionProAlg = (CogSerializer.LoadObjectFromFile(Application.StartupPath+"/XXX.vpp") as CogToolBlock);
+                visionProAlg.Ran += VisionAlgorithm_OnRan;
+
+            }
+            MessageBox.Show("完成初始化");
+        }
+
+
+        public void DisplayResult(object disp)
+        {
+            CogRecordDisplay Display = disp as CogRecordDisplay;
+            CogToolBlock checker = visionProAlg;
+            if (Display.InvokeRequired)
+            {
+                Display.BeginInvoke((Action)delegate
+                {
+                    DisplayResult(Display, checker);
+                });
+            }
+            else
+            {
+                DisplayResult(Display, checker);
+            }
+        }
+        private void DisplayResult(CogRecordDisplay Display, CogToolBlock checker)
+        {
+            if (checker.RunStatus.Result != 0)
+            {
+                Display.Image = (checker.Outputs["OutputImage"].Value as Cognex.VisionPro.ICogImage);
+                Display.Fit(false);
+            }
+            else
+            {
+                Cognex.VisionPro.ICogRecord record = checker.CreateLastRunRecord();
+                Display.Image = (checker.Outputs["OutputImage"].Value as Cognex.VisionPro.ICogImage);
+                Display.Record = record;
+                Display.Fit(false);
+            }
+        }
+        private void VisionAlgorithm_OnRan(object sender, EventArgs e)
+        {
+            DisplayResult(this.MainDisplay);
+
+            if (visionProAlg.RunStatus.Result != CogToolResultConstants.Accept)
+            {
+                MyTCPServer.Send("NoData");
+                objScanState = ScanState.NODATA;
+            }
+            else
+            {
+
+               DealDataSend(visionProAlg);
+               
+                objScanState = ScanState.SUCCEED;
+            }
+        }
+
+
+        private void DealDataSend(CogToolBlock checker)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() =>
+                {
+                    DisplayData(checker);
+
+                }));
+            }
+            else
+            {
+                DisplayData(checker);
+            }
+
+
+        }
+        double Wight;
+        private void DisplayData(CogToolBlock checker)
+        {
+            Wight = Convert.ToDouble(checker.Outputs["Width"].Value);
+            label2.Text = Wight.ToString("F2");
+        }
+
+        DateTime lastScanTime;
+        enum ScanState
+        {
+
+            INITIAL,
+            OVERTIME,
+            AWAIT,
+            SCANNING,
+            NODATA,
+            SUCCEED,
+            ERROR
+        }
+        ScanState objScanState
+        {
+            get
+            {
+                return _objScanState;
+            }
+            set
+            {
+                if (_objScanState != value)
+                {
+                    _objScanState = value;
+                    NotifierHelper.Notify("ScanState.Changed", _objScanState);
+                }
+            }
+        }
+        ScanState _objScanState = ScanState.INITIAL;
+        private void SrIs_OnPackageImage(object theImage)
+        {
+            if (Sr_status == SensorStatus.Idle)
+            {
+                NotifierHelper.Notify("Sensor.StartScan", "启动扫描。");
+                objScanState = ScanState.SCANNING;
+                Sr_status = SensorStatus.Scaning;
+                lastScanTime = DateTime.Now;
+
+                watchOn = new Thread(SensorWatchOn);
+                watchOn.Start();
+                //SR_timer.Interval = 8000;
+                //SR_timer.Start();
+            }
+            this.LiveDisplay.Image = theImage as ICogImage;
+            LiveDisplay.Fit();
+        }
+
+        private void SensorWatchOn()
+        {
+            NotifierHelper.Notify("Sensor.SensorWatchOn", "扫描监视");
+            while (true)
+            {
+                TimeSpan x = DateTime.Now - lastScanTime;
+                if (x.TotalMilliseconds > 6000)
+                {
+                    if (Sr_status == SensorStatus.Scaning)
+                    {
+                        NotifierHelper.Notify("Sensor.ScanTimeOut", "扫描超时。");
+                        {
+                            SmartRayImageSource.Stop();
+                            Sr_status = SensorStatus.Aborting;
+                            Thread.Sleep(500);
+                            SmartRayImageSource.ImageSource srIs = SmartRayImageSource as SmartRayImageSource.ImageSource;
+                            srIs.ClearDatas();
+                            SmartRayImageSource.SoftTrigger();
+                            objScanState = ScanState.OVERTIME;
+                            //Connection2.Send("OverTime");
+                            NotifierHelper.Notify("ImageSource.SoftTriger");
+                        }
+                    }
+                    break;
+
+                }
+                Thread.Sleep(100);
+            }
+            //watchOn.Abort();
+
+            NotifierHelper.Notify("Sensor.SensorWatchOnStop", "扫描监视结束");
+
+        }
+        Thread watchOn;
+        SensorStatus Sr_status = SensorStatus.Idle;
+        private void ImageSource_OnImage(Object theImage)
+        {
+            if (watchOn != null)
+            {
+                watchOn.Abort();
+            }
+
+            if (Sr_status == SensorStatus.Aborting)
+            {
+                SmartRayImageSource.SoftTrigger();
+                NotifierHelper.Notify("ImageSource.SoftTriger");
+            }
+            else
+
+            {
+                NotifierHelper.Notify("Sensor.OnImage", "获得图像。");
+
+                object[] imgs = theImage as object[];
+
+                this.LiveDisplay.Image = imgs[0] as ICogImage;
+                LiveDisplay.Fit();
+
+                visionProAlg.Inputs["ZMap"] .Value= imgs[0];
+                visionProAlg.Inputs["Intensity"].Value = imgs[1];
+                visionProAlg.Run();
+                //this.VisionProject.VisionAlgorithm.Inputs["ZMap"] = imgs[0];
+                //this.VisionProject.VisionAlgorithm.Inputs["Intensity"] = imgs[1];
+                //this.VisionProject.VisionAlgorithm.Run();
+            }
+
+            Sr_status = SensorStatus.Idle;
+
+        }
+
+
+
+
+        private int client_count = 0;
+        private void Connection2_OnConnect(TcpServerConnection connection)
+        {
+            client_count++;
+            NotifierHelper.Notify("Connection.ClientIn");
+
+            //if (client_count > 0)
+            {
+                UpdateClientStatus();
+
+            }
+        }
+        private void Connection2_OnLeaving(TcpServerConnection connection)
+        {
+            client_count--;
+            NotifierHelper.Notify("Connection.ClientLeave", "客户端断开。");
+            UpdateClientStatus();
+        }
+        private void UpdateClientStatus()
+        {
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new Action(
+                    () =>
+                    {
+                        this.cbClient.Checked = (client_count > 0);
+                    }
+                    ));
+            }
+            else
+            {
+                this.cbClient.Checked = client_count > 0;
+
+            }
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            SmartRayImageSource.SoftTrigger();
+            NotifierHelper.Notify("ImageSource.SoftTriger");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            CogComponentEditorForm cfgWnd = new CogComponentEditorForm(visionProAlg, false, true);
+            cfgWnd.Show();
+            cfgWnd.BringToFront();
         }
     }
 }
